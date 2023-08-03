ae1cf8708749d210347be158176869727bdabfb2
developerankit97 <ankitsh.developer@gmail.com>
2023-08-03 11:38:21 +0530

    third commit

--- public/home.js
+++ public/home.js
@@ -4,7 +4,7 @@ btnInsta.addEventListener('click', loginWithInstagram);
 
 function loginWithInstagram () {
     console.log('working');
-    let appId = "300029245736907";
+    let appId = "299541432560866";
 	let redUri = window.location.origin + "/insta";
     // let redUri = 'https://google.com/'
 	let url = `https://api.instagram.com/oauth/authorize?client_id=${appId}&redirect_uri=${redUri}&scope=user_profile,user_media&response_type=code`;
