bee58ca6aaf979f0f7620c1d9fa54a549bc4f7a1
developerankit97 <ankitsh.developer@gmail.com>
2023-08-03 12:25:21 +0530

    fifth commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,27 @@
-
+const axios = require('axios');
 
 exports.instaLogin = async (req, res, next) => {
+    const {code} = req.query;
+    console.log('working', req.params, code);
+    try {
+        const response = await axios.post('https://your-server-url/init-insta', {
+            url: 'https://api.instagram.com/oauth/access_token',
+            form: {
+                client_id: 299541432560866,
+                client_secret: c47f80c1e8faa2372586dfd1cc9ac91b,
+                grant_type: 'authorization_code',
+                redirect_uri: 'https://google.com/',
+                code: code
+            }
+        });
     
-    console.log('working', req.params, req.query);
+        // Handle the response from your server as needed
+        console.log(response.data, response);
+        res.send('POST request sent successfully');
+      } catch (error) {
+        // Handle any errors that occurred during the POST request
+        console.error(error);
+        res.status(500).send('Error sending POST request');
+      }
     res.status(200).json({status: 'success'});
 }
