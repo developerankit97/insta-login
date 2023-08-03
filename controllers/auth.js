0d32ce8669a40df2d4745bd73c826bc091c3fdfc
developerankit97 <ankitsh.developer@gmail.com>
2023-08-03 13:24:26 +0530

    eleventh commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,15 +11,9 @@ exports.instaLogin = async (req, res, next) => {
         data.append('grant_type', 'authorization_code');
         data.append('redirect_uri', 'https://insta-a9e6.onrender.com/insta'); // Replace with your registered redirect URI
         const response = await axios.post('https://api.instagram.com/oauth/access_token', data );
-        // {
-        //     code: code,
-        //     client_id: "299541432560866",
-        //     client_secret: "c47f80c1e8faa2372586dfd1cc9ac91b",
-        //     grant_type: 'authorization_code',
-        //     redirect_uri: 'https://insta-a9e6.onrender.com/insta',
-        // }
-        // Handle the response from your server as needed
-        console.log(response.data, response);
+        console.log(response.data,);
+        let resp = await axios.get(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=c47f80c1e8faa2372586dfd1cc9ac91b&access_token=${response.data.access_token}`)
+        console.log(resp.data, '***************************resp data***********');
         res.send('POST request sent successfully');
       } catch (error) {
         // Handle any errors that occurred during the POST request
